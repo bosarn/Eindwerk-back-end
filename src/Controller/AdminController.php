d8b4d52f68d415d785f3fcbe985a7ce6b6983bbf
bosarno <arnobosmans1993@gmail.com>
2020-05-24 14:31:57 +0200

    -Admin controls  -- object View + functions

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -7,10 +7,12 @@ use App\Entity\Printer;
 use App\Entity\Files;
 use App\Repository\OrdersRepository;
 use App\Repository\OrderDetailsRepository;
+use App\Repository\PrintedobjectRepository;
 use App\Entity\OrderDetails;
 use App\Entity\Orders;
 use App\Entity\Printedobject;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,13 +21,23 @@ use Symfony\Component\HttpClient\HttpClient;
 
 class AdminController extends AbstractController
 {
+    protected $requestStack;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
+
     /**
      * @Route("/admin/objects", name="app_admin_objects")
+     * @param PrintedobjectRepository $repository
      * @return Response
      */
-    public function objects()
+    public function objects(PrintedobjectRepository $repository)
     {
-        return $this->render('admin/objects.html.twig');
+        $objects = $repository->findAll();
+        return $this->render('admin/objects.html.twig', ['objects' => $objects]);
     }
 
     /**
@@ -68,7 +80,45 @@ public function sendFileToPrinter($fil,$printe){
     // adjust file to quantity minus 1
     // redirects
     //Printer $printer, Files $files
-    dd($fil,$printe);
-}
+
 }
 
+    /**
+     * @Route("/admin/object/add", name="add_object")
+     * @return Response
+     */
+    public function addobject (Request $rokrest){
+
+    $rokrest->get('object_name');
+        $request = $this->requestStack->getCurrentRequest();
+         dd($request->getQueryString());
+         dd($request->request->all());
+        if ($request->getMethod() == "POST"){
+            echo $request->getContent();
+
+
+            return new Response('Saved new product with id ');
+        }
+        dd($request);
+        return new Response('Not posted');
+    }
+}
+
+/**
+ *
+$entityManager = $this -> getDoctrine() ->getManager();
+
+$object = new Printedobject();
+
+$object->setName($name);
+$object->setPrintTime($printtime);
+$object->setSize($size);
+foreach($files as $file){
+$object->addFile($file);
+}
+
+$entityManager->persist($object);
+$entityManager->flush();
+
+
+ */
